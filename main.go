4c09369be8f338ba74512b73e71cb1689bc80a64
Evgeniy Yablokov <ey@odoscope.com>
2019-12-17 16:15:32 +0100

    add Backend struct

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ import (
 )
 
 
+// Backend holds the data about server
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
 func main() {
 
 }
