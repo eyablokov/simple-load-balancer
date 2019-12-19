e765cecd01e6519d5a9a36818e285e7bd835f7aa
Evgeniy Yablokov <ey@odoscope.com>
2019-12-19 17:37:40 +0100

    `lb`load balancer

--- main.go
+++ main.go
@@ -67,6 +67,16 @@ func (s *ServerPool) GetNextPeer() *Backend {
 	return nil
 }
 
+// lb balances icoming requests
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := serverPool.GetNextPeer()
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
 func main() {
 
 }
